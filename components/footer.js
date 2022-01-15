d1d014ff5c8372ad1eb28ca402423223600469c1
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-14 21:33:03 -0500

    footer component

---
+++ components/footer.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+//import Link from "next/link";
+import styles from '../styles/Home.module.css'
+export default function Footer () {
+const footTitle = 'DED Next Portfolio'
+  return (
+    <>
+      <footer className={styles.footer}>
+        <h1>{footTitle}</h1>
+        <a href="https://github.com/bahim22" target="_blank" rel="noopener noreferrer" />
+        Dionysus
+          <a href="https://github.com/bahim22" target="_blank" rel="noopener noreferrer">
+            GULLO{' '}
+            <span className={styles.logo}>
+              <Image src="/logo.svg" alt="Dionysus Logo" width={125} height={50} />
+            </span>
+          </a>
+      </footer>
+    </>
+  )
+}
+
+//OldFoot.defaultProps = Image({...all}, ImageProps)
